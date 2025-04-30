7ce6b151cd77b63bdc09ab7c6cb0cb7e0970933a
uiel <parkuiery@gmail.com>
2025-04-30 12:22:54 +0900

    feat :: MainActivity 구현

--- app/src/main/java/com/uiel/matzo/MainActivity.kt
+++ app/src/main/java/com/uiel/matzo/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,28 +19,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             MatzoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MatzoApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MatzoTheme {
-        Greeting("Android")
+        MatzoApp()
     }
 }
